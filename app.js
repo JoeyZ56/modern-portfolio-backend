7ad72c2d8229e3e4a593e15c14092878051b813d
JoeyZ56 <jzazzi56@gmail.com>
2025-04-23 15:13:21 -0700

    testing if serveless setup is causing the timeout

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
+// const express = require("express");
+// const serverless = require("serverless-http");
+// const dotenv = require("dotenv");
+// const cors = require("cors");
+// const connectDB = require("./config/db");
+
+// //Imported Routes
+// const projectRoutes = require("./routes/projectRoutes");
+// const SendEmailRoutes = require("./routes/emailRoute");
+
+// dotenv.config();
+// connectDB();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// //Routes
+// app.use("/api/projects", projectRoutes);
+// app.use("/api/send-email", SendEmailRoutes);
+
+// app.get("/", (req, res) => {
+//   res.status(200).send("API is running...");
+// });
+
+// //Server Start
+// // app.listen(PORT, () => {
+// //   console.log(`A wild server has appeared!`);
+// // });
+
+// module.exports = serverless(app);
+
+//Test
+
 const express = require("express");
 const serverless = require("serverless-http");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const connectDB = require("./config/db");
-
-//Imported Routes
-const projectRoutes = require("./routes/projectRoutes");
-const SendEmailRoutes = require("./routes/emailRoute");
-
-dotenv.config();
-// connectDB();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-//Routes
-app.use("/api/projects", projectRoutes);
-app.use("/api/send-email", SendEmailRoutes);
-
 app.get("/", (req, res) => {
-  res.status(200).send("API is running...");
+  res.status(200).send("It works! 🎉");
 });
-
-//Server Start
-// app.listen(PORT, () => {
-//   console.log(`A wild server has appeared!`);
-// });
 
 module.exports = serverless(app);
