42abdc5e2e8ca13a55c0e7b83a24348d1210e4a4
JoeyZ56 <jzazzi56@gmail.com>
2025-04-25 12:59:53 -0700

    moving away from vercels serverless style to dedicated port

--- app.js
+++ app.js
@@ -1,45 +1,29 @@
-// const express = require("express");
-// const serverless = require("serverless-http");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-// const connectDB = require("./config/db");
-
-// //Imported Routes
-// const projectRoutes = require("./routes/projectRoutes");
-// const SendEmailRoutes = require("./routes/emailRoute");
-
-// dotenv.config();
-// connectDB();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// //Routes
-// app.use("/api/projects", projectRoutes);
-// app.use("/api/send-email", SendEmailRoutes);
-
-// app.get("/", (req, res) => {
-//   res.status(200).send("API is running...");
-// });
-
-// //Server Start
-// // app.listen(PORT, () => {
-// //   console.log(`A wild server has appeared!`);
-// // });
-
-// module.exports = serverless(app);
-
-//Test
-
 const express = require("express");
-const serverless = require("serverless-http");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+//Imported Routes
+const projectRoutes = require("./routes/projectRoutes");
+const SendEmailRoutes = require("./routes/emailRoute");
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use("/api/projects", projectRoutes);
+app.use("/api/send-email", SendEmailRoutes);
+
 app.get("/", (req, res) => {
-  res.status(200).send("It works! 🎉");
+  res.status(200).send("A wild server has appeared!");
 });
 
-module.exports = serverless(app);
+// Listen to requests
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
